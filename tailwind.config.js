577a086213a5724325d2fa61fd18bc8af0ce7688
Zubair Shaik <zubyshaik08@gmail.com>
2021-11-27 18:52:58 -0600

    finished sign in page

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        gray: {
+          darkest: '#1f2d3d',
+          dark: '#3c4858',
+          DEFAULT: '#242424',
+          light: '#e0e6ed',
+          lightest: '#f9fafc',
+        }
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
