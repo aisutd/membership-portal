3c7252e50ccf1bc704c2aa5be57d9a3a5acf47be
Harsha Srikara <harshasrikara@gmail.com>
2022-02-05 00:10:05 -0600

    refactor: create env manager (#20)

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,15 +2,16 @@ import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
 import { DynamoDBAdapter } from "@next-auth/dynamodb-adapter";
 import AWS from "util/aws";
+import env from "util/env";
 import { updateProfile, profile_update_schema } from "util/db/profile";
 
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     Providers.Cognito({
-      clientId: process.env.COGNITO_CLIENT_ID,
-      clientSecret: process.env.COGNITO_CLIENT_SECRET,
-      domain: process.env.COGNITO_DOMAIN,
+      clientId: env.COGNITO_CLIENT_ID,
+      clientSecret: env.COGNITO_CLIENT_SECRET,
+      domain: env.COGNITO_DOMAIN,
     }),
     // ...add more providers here
   ],
